4a8f82813d2e401fa9c0ac2f95688c3df9c9068d
Jenna or JE <74290606+whatthehaaa@users.noreply.github.com>
2023-07-21 14:47:30 +0900

    Update SessionVO.java
    ff

--- spring_mycgv/src/main/java/com/mycgv/vo/SessionVO.java
+++ spring_mycgv/src/main/java/com/mycgv/vo/SessionVO.java
@@ -1,21 +1,2 @@
 package com.mycgv.vo;
 
-public class SessionVO {
-	int result;
-	String name;
-	
-	public int getResult() {
-		return result;
-	}
-	public void setResult(int result) {
-		this.result = result;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	
-}
